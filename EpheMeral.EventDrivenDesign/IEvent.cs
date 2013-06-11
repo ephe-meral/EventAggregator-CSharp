1f275729917507dfd0730810942f6f128be575aa
Johanna Appel <j.appel.online@gmail.com>
2013-06-12 00:50:22 +0200

    fixed typo

--- EpheMeral.EventDrivenDesign/IEvent.cs
+++ EpheMeral.EventDrivenDesign/IEvent.cs
@@ -9,7 +9,7 @@ namespace EpheMeral.EventDrivenDesign
 	/// <summary>
 	/// IEvent interface.
 	/// This interface can be used to "tag" a certain class to be
-	/// an interface. Its just for convenience so we dont have to
+	/// an event. Its just for convenience so we dont have to
 	/// use "object" as a type for events.
 	/// </summary>
 	public interface IEvent
